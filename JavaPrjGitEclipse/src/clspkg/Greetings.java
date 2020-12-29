d2bbabb677bd38a22182880d75013d047cc47e85
ronanshields <ronanshields@ubuntu>
2020-12-29 18:02:06 +0000

    A change was made to Greetings.java

---
+++ JavaPrjGitEclipse/src/clspkg/Greetings.java
@@ -0,0 +1,10 @@
+package clspkg;
+
+public class Greetings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Geetings LYIT student");
+	}
+
+}
