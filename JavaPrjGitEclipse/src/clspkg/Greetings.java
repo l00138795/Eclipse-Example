a0a987bb87ec2b7b1882b5de5697fa12acd55ffc
ronanshields <ronanshields@ubuntu>
2020-12-29 18:09:44 +0000

    Changed greeting message

--- JavaPrjGitEclipse/src/clspkg/Greetings.java
+++ JavaPrjGitEclipse/src/clspkg/Greetings.java
@@ -4,7 +4,7 @@ public class Greetings {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Geetings LYIT student");
+		System.out.println("Greetings LYIT student and happy christmas");
 	}
 
 }
